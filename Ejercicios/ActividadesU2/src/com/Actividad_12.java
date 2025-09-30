3f8767632c824d8a526b569b1c71dd7b5b38e17c
Sora368 <martinez.bocanegra.alberto.luis@iescamas.es>
2025-09-30 20:39:56 +0200

    Actividad_12.java

--- Ejercicios/ActividadesU2/src/com/Actividad_12.java
+++ Ejercicios/ActividadesU2/src/com/Actividad_12.java
@@ -1,55 +1,57 @@
 package com;
 import java.util.Scanner;
-public class Actividad_10 {
+public class Actividad_12 {
 
 	public static void main(String[] args) {
-		// Declaramos las variables
+		/**
+		 * Pedir el día, mes y año de una fecha e
+		 *  indicar si la fecha es correcta. Recordamos 
+		 *  que existen meses con 28, 30 y 31 días. 
+		 *  No consideraremos los años bisiestos.
+		 								*/
+		//Declaramos las variables
+		int dia, mes, año;
 		Scanner sc = new Scanner(System.in);
-		int numero, unidades, decenas, centenas, millares;
 		
-		/**
-		 * Pedimos un número
-		 */
-		System.out.println("Introduce un número");
 		
-		numero = sc.nextInt();
-		/**
-		 * 
-		 * 
-		 * Escribimos las condicionales
-		 * 
-		 */
+		// Pedimos la fecha
+		System.out.println("Introduce el dia");
+		dia = sc.nextInt();
 		
-		if (numero < 10){
-			System.out.println("No es un número capicua");
-		}else if (numero <100) {
-			unidades = numero % 10;
-			decenas = numero / 10;
-			if (unidades == decenas){
-				System.out.println(numero +" es capicúa");
-			}else {
-				System.out.println(numero +" no es capicúa");
-			}
-		}else if (numero < 1000) {
-			unidades = numero % 10;
-			centenas = numero / 100;
-			if (unidades == centenas) {
-				System.out.println(numero +" es capicúa");
-			}else {
-				System.out.println(numero +" no es capicúa");
-			}
-		}else if (numero < 10000) {
-			unidades = numero % 10;
-			decenas = (numero / 10)%10;
-			centenas = (numero / 100) % 10;
-			millares = numero / 1000;
-			if (unidades == millares && decenas == centenas) {
-				System.out.println(numero +" es capicúa");
-			}else {
-				System.out.println(numero + " no es capicúa");
-			}
+		System.out.println("Introduce el mes");
+		mes = sc.nextInt();
+		
+		System.out.println("Introduce el año");
+		año = sc.nextInt();
+		
+		//Hacemos las condicionales
+		if ((mes == 1)&&(dia>= 1 && dia<=31)){
+			System.out.println("Es una fecha válida");
+			
+		}else if ((mes == 2)&&(dia>=1 && dia<= 28)) {
+			System.out.println("Es una fecha válida");
+		}else if ((mes == 3)&& (dia >= 1 && dia <=31)){
+			System.out.println("Es una fecha válida");
+		}else if ((mes ==4 )&&(dia >=1 && dia <= 30)){
+			System.out.println("Es una fecha válida");
+		}else if ((mes == 5)&&(dia >=1 && dia <= 31)){
+			System.out.println("Es una fecha válida");
+		}else if ((mes == 6)&&(dia >=1 && dia <=30 )){
+			System.out.println("Es una fecha válida");
+		}else if ((mes == 7)&&(dia >=1 && dia<= 31)) {
+			System.out.println("Es una fecha válida");
+		}else if ((mes == 8)&&(dia >= 1 && dia<= 30)){
+			System.out.println("Es una fecha válida");
+		}else if ((mes == 9)&&( dia >= 1 && dia<= 31)){
+			System.out.println("Es una fecha válida");
+		}else if ((mes == 10)&&( dia>=1 && dia <=30)){
+			System.out.println("Es una fecha válida");
+		}else if ((mes ==11) && ( dia >=1 && dia <= 31)){
+			System.out.println("Es una fecha válida");
+		}else if ((mes==12)&&(dia >= 1 && dia <= 30)) {
+			System.out.println("Es una fecha válida");
 		}else {
-			System.out.println("Es un número demasiado grande");
+			System.out.println("No es una fecha válida");
 		}
 	}
 
