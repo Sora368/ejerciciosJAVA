8834a93dd57390699d5b0ce7e0516acc37736d7c
Sora368 <martinez.bocanegra.alberto.luis@iescamas.es>
2025-09-30 20:39:12 +0200

    Actividad_12.java

--- Ejercicios/ActividadesU2/src/com/Actividad_12.java
+++ Ejercicios/ActividadesU2/src/com/Actividad_12.java
@@ -1,10 +1,56 @@
 package com;
-
-public class Actividad_12 {
+import java.util.Scanner;
+public class Actividad_10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Declaramos las variables
+		Scanner sc = new Scanner(System.in);
+		int numero, unidades, decenas, centenas, millares;
+		
+		/**
+		 * Pedimos un número
+		 */
+		System.out.println("Introduce un número");
+		
+		numero = sc.nextInt();
+		/**
+		 * 
+		 * 
+		 * Escribimos las condicionales
+		 * 
+		 */
+		
+		if (numero < 10){
+			System.out.println("No es un número capicua");
+		}else if (numero <100) {
+			unidades = numero % 10;
+			decenas = numero / 10;
+			if (unidades == decenas){
+				System.out.println(numero +" es capicúa");
+			}else {
+				System.out.println(numero +" no es capicúa");
+			}
+		}else if (numero < 1000) {
+			unidades = numero % 10;
+			centenas = numero / 100;
+			if (unidades == centenas) {
+				System.out.println(numero +" es capicúa");
+			}else {
+				System.out.println(numero +" no es capicúa");
+			}
+		}else if (numero < 10000) {
+			unidades = numero % 10;
+			decenas = (numero / 10)%10;
+			centenas = (numero / 100) % 10;
+			millares = numero / 1000;
+			if (unidades == millares && decenas == centenas) {
+				System.out.println(numero +" es capicúa");
+			}else {
+				System.out.println(numero + " no es capicúa");
+			}
+		}else {
+			System.out.println("Es un número demasiado grande");
+		}
 	}
 
 }
